69e0e756d093f599243ee0685b6436ebfe119269
Claudio Scheer <claudioscheer@protonmail.com>
2020-03-26 14:35:32 -0300

    Install PDF Shuffler

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -3,7 +3,7 @@
 sudo apt update
 sudo apt upgrade -y
 
-sudo apt install git curl wget keepassxc ca-certificates apt-transport-https uget krdc libreoffice gnucash vim build-essential speedcrunch obs-studio telegram-desktop gimp -y
+sudo apt install git curl wget keepassxc ca-certificates apt-transport-https uget krdc libreoffice gnucash vim build-essential speedcrunch obs-studio telegram-desktop gimp pdfshuffler -y
 
 # Miniconda
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
