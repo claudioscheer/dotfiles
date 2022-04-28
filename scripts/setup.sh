210d23b5043b97748cd75fa66d612909ce5f6330
Claudio Scheer <claudioscheer@protonmail.com>
2022-04-28 01:30:03 -0300

    Update setup script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -37,23 +37,9 @@ sudo apt install -y \
     net-tools \
     linux-tools-common \
     linux-tools-generic \
-    linux-tools-`uname -r`
-
-# Neovim
-sudo add-apt-repository ppa:neovim-ppa/unstable
-sudo apt install neovim -y
-sudo ln -s /usr/bin/nvim /usr/bin/vim
-
-# Alacritty
-sudo add-apt-repository ppa:mmstick76/alacritty -y
-sudo apt-get update
-sudo apt install alacritty -y
-
-# Syncthing
-curl -s https://syncthing.net/release-key.txt | sudo apt-key add -
-echo "deb https://apt.syncthing.net/ syncthing stable" | sudo tee /etc/apt/sources.list.d/syncthing.list
-sudo apt update
-sudo apt install syncthing -y
+    linux-tools-`uname -r` \
+    neovim \
+    syncthing
 
 ## DBeaver
 #wget https://dbeaver.io/files/dbeaver-ce_latest_amd64.deb -O dbeaver.deb
@@ -68,7 +54,7 @@ sudo apt install syncthing -y
 #tar -zxvf postman.tar.gz
 #sudo mv Postman /opt/
 
-# Node.js v12.x
+# Node.js v14.x
 curl -fsSL https://deb.nodesource.com/setup_14.x | sudo -E bash -
 sudo apt install -y nodejs
 mkdir ~/.npm-global
@@ -120,32 +106,10 @@ curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip
 unzip awscliv2.zip
 sudo ./aws/install
 
-# Rambox CE
-wget https://github.com/ramboxapp/community-edition/releases/download/0.7.7/Rambox-0.7.7-linux-amd64.deb -O rambox.deb
-sudo dpkg -i rambox.deb
-sudo apt -f install -y
-
 # yEd
 wget https://www.yworks.com/resources/yed/demo/yEd-3.21.1_with-JRE15_64-bit_setup.sh -O yed.sh
 chmod +x yed.sh
 ./yed.sh
-
-# adblock
-wget https://raw.githubusercontent.com/MattiSG/adblock/master/adblock -O adblock
-chmod +x adblock
-sudo mv adblock /usr/local/bin/
-sudo adblock on
-
-# TexLab
-wget https://github.com/latex-lsp/texlab/releases/download/v2.2.2/texlab-x86_64-linux.tar.gz -O texlab.tar.gz
-tar -zxvf texlab.tar.gz
-sudo mv texlab /usr/local/bin
-
-# ActivityWatch
-wget https://github.com/ActivityWatch/activitywatch/releases/download/v0.10.0/activitywatch-v0.10.0-linux-x86_64.zip -O activitywatch.zip
-unzip activitywatch.zip
-sudo mv activitywatch /opt/activitywatch
-sudo ln -s /opt/activitywatch/aw-qt.desktop ~/.local/share/applications/aw-qt.desktop
 
 # Zoom
 wget https://cdn.zoom.us/prod/5.6.13632.0328/zoom_amd64.deb -O zoom.deb
@@ -157,22 +121,10 @@ wget https://download-cf.jetbrains.com/fonts/JetBrainsMono-2.225.zip -O JetBrain
 unzip JetBrainsMono.zip
 sudo mv fonts/ttf/ /usr/local/share/fonts/jetbrains-mono
 
-# Nextcloud CLI
-sudo add-apt-repository ppa:nextcloud-devs/client
-sudo apt-get update
-sudo apt install nextcloud-client
-
 # Final upgrade
 sudo apt update
 sudo apt upgrade -y
 sudo apt autoremove -y
-
-# Configure Neovim
-cp -r ../.config ~
-sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-vim -c ":PlugInstall" -c q -c q
-mkdir ~/.vim/undodir -p
 
 # Miniconda
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
